eb128e13a22a793927ab6fdbd345ada54c8c561a
HHameem <hhameem@my.centennialcollege.ca>
2025-04-04 19:05:55 -0400

    Add files via upload

---
+++ StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringExtensions
+{
+    public static class StringExtensions
+    {
+        public static bool StartsWithUpper(this string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return false;
+            return char.IsUpper(str[0]);
+        }
+    }
+}
